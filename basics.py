00a939c29af7fc30381aeba3eefe4ee894143e70
Venu Gopal <venugopal.creatives@gmail.com>
2026-06-30 14:44:07 +0530

    lesson 1

---
+++ basics.py
@@ -0,0 +1,8 @@
+
+# bascis pytho practice 
+name = input (" enter ur name: ")
+age = int(input("enter your age: "))    
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")  
