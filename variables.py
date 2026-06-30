a92a1c1584fd484ea45e006b32f2c422c3394522
Venu Gopal <venugopal.creatives@gmail.com>
2026-06-30 15:03:44 +0530

    variables 1

--- variables.py
+++ variables.py
@@ -1,1 +1,26 @@
 print ("hey ok lets start") 
+name = "venu"
+age = 34
+Height = 6.4
+is_sexy = True
+
+print(name)
+print(age)
+print(Height)
+print(is_sexy)  
+
+print(type(name))
+print(type(age))
+print(type(Height))
+print(type(is_sexy))    
+
+price = 100
+qty = 5
+total = price * qty
+print(total)    
+
+city, state, country = "Bangalore", "Karnataka", "India"
+
+print("city:", city)
+print("state:", state)
+print("country:", country)  
